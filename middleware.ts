c72d2563a97e44598e96e5b0a10cf99fbe75ab92
Facundo Martin <facundomartin98@gmail.com>
2024-01-20 12:10:39 -0800

    feat: integrate clerk orgs

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,42 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/", "/contact"],
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // Handle authed users who are not inside an organization page
+    if (auth.userId && auth.isPublicRoute) {
+      const path = auth.orgId ? `/organization/${auth.orgId}` : "/select-org";
+      const orgSelection = new URL(path, req.url);
+
+      return NextResponse.redirect(orgSelection);
+    }
+
+    // Redirect logged in users to organization selection page if they are not active in an organization
+    if (
+      auth.userId &&
+      !auth.orgId &&
+      req.nextUrl.pathname !== "/org-selection"
+    ) {
+      const orgSelection = new URL("/org-selection", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (auth.userId && auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    // Allow users visiting public routes to access them
+    return NextResponse.next();
+  },
 });
 
 export const config = {
